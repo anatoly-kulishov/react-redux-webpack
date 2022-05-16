05ccf6c1f064618cd8d9ead463e1d77beaaeb0d8
Anatoly.Kulishov <Anatoly.Kulishov@waveaccess.ru>
2022-05-16 16:56:38 +0300

    1.0.0

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import "./App.scss";
+
+export const App = () => {
+  return (
+    <div className="AppWrapper">
+      <h1 className="AppTitle">It's alive!!!</h1>
+    </div>
+  )
+}
